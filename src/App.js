8df5fa364961c1e6a635242f6d3f7c2e3fd2dee9
Va5s0 <vassog@gmail.com>
2018-10-14 18:37:44 +0300

    add search button to main page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,87 @@
+import * as R from "ramda"
 import React, { Component } from "react"
 import Header from "./components/Header"
-import { Grid, Row, Col, Button, Modal, Glyphicon } from "react-bootstrap"
+import {
+  Grid,
+  Row,
+  Col,
+  Button,
+  Modal,
+  Glyphicon,
+  FormGroup,
+  FormControl,
+} from "react-bootstrap"
+import AppActions from "./actions/AppActions"
+import AppStore from "./stores/AppStore"
 import AddParent from "./components/AddParent"
 import Parents from "./components/Parents"
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
+import { css } from "emotion"
+
+const classNames = {
+  containerDiv: css`
+    display: flex;
+    justify-content: space-between;
+    margin: 15px;
+  `,
+  formControl: css`
+    border: 1px solid #668887;
+    height: 36px;
+  `,
+}
+
+let FILTER_TIMER_ID = undefined
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
+      filteredParents: undefined,
       openAdd: false,
+      searchField: "",
+      parents: [],
     }
+  }
+
+  componentWillMount() {
+    AppStore.addChangeListener(this.onChange)
+  }
+
+  componentDidMount() {
+    AppActions.getParents()
+    const { parents, filterData } = this.state
+
+    if (!filterData && parents && parents.length) {
+      this.setState(state => ({ ...state, filterData: parents }))
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { parents, filteredParents, searchField } = this.state
+    const { searchField: pSearchField } = prevState
+
+    if (!filteredParents && parents && parents.length) {
+      this.setState(state => ({ ...state, filteredParents: parents }))
+    }
+
+    if (searchField !== pSearchField) {
+      FILTER_TIMER_ID && clearTimeout(FILTER_TIMER_ID)
+      FILTER_TIMER_ID = setTimeout(this.filterData, 300)
+    }
+  }
+
+  componentWillUnmount() {
+    AppStore.removeChangeListener(this.onChange)
+    FILTER_TIMER_ID && clearTimeout(FILTER_TIMER_ID)
+  }
+
+  onChange = () => this.setState({ parents: AppStore.getParents() })
+
+  onSearchChange = evt => {
+    const searchField = evt.target.value
+    this.setState(state => ({ ...state, searchField }))
   }
 
   closeAdd = () => {
@@ -27,6 +96,31 @@ class App extends Component {
     this.closeAdd()
   }
 
+  filterData = () => {
+    const { parents, searchField } = this.state
+    const sf = searchField.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&")
+    const searchRegex = new RegExp(sf, "gi")
+    let p = []
+    p = R.filter(
+      R.anyPass([
+        R.compose(
+          R.test(searchRegex),
+          R.path(["firstName"])
+        ),
+        R.compose(
+          R.test(searchRegex),
+          R.path(["lastName"])
+        ),
+        R.compose(
+          R.test(searchRegex),
+          R.path(["phone"])
+        ),
+      ]),
+      parents
+    )
+    this.setState(state => ({ ...state, filteredParents: p }))
+  }
+
   render() {
     return (
       <div className="App">
@@ -34,26 +128,37 @@ class App extends Component {
         <Grid>
           <Row>
             <Col xs={12} md={12} lg={12}>
-              <div>
-                <Button
-                  id="addButton"
-                  className="custom"
-                  onClick={this.openAdd}
-                >
-                  <Glyphicon glyph="glyphicon glyphicon-plus" />
-                </Button>
-                <Modal
-                  show={this.state.openAdd}
-                  onHide={this.closeAdd}
-                  keyboard={true}
-                >
-                  <Modal.Body className="add-modal">
-                    <AddParent callbackParent={this.onParentChanged} />
-                  </Modal.Body>
-                </Modal>
+              <div className={classNames.containerDiv}>
+                <div>
+                  <Button className="custom" onClick={this.openAdd}>
+                    <Glyphicon glyph="glyphicon glyphicon-plus" />
+                  </Button>
+                  <Modal
+                    show={this.state.openAdd}
+                    onHide={this.closeAdd}
+                    keyboard={true}
+                  >
+                    <Modal.Body className="add-modal">
+                      <AddParent callbackParent={this.onParentChanged} />
+                    </Modal.Body>
+                  </Modal>
+                </div>
+                <div>
+                  <form>
+                    <FormGroup
+                      controlId={"formControlsText"}
+                      onChange={this.onSearchChange}
+                    >
+                      <FormControl
+                        className={classNames.formControl}
+                        placeholder="search"
+                      />
+                    </FormGroup>
+                  </form>
+                </div>
               </div>
               <div>
-                <Parents />
+                <Parents parents={this.state.filteredParents} />
               </div>
             </Col>
           </Row>
--- src/components/Parents.js
+++ src/components/Parents.js
@@ -1,40 +1,15 @@
-import React, { Component } from "react"
+import React from "react"
 import ParentListItem from "./ParentListItem"
-import AppActions from "../actions/AppActions"
-import AppStore from "../stores/AppStore"
 
-class Parents extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      parents: [],
-    }
+const Parents = ({ parents }) => {
+  let parentListItems
+
+  if (parents) {
+    parentListItems = parents.map(parent => {
+      return <ParentListItem key={parent._id.$oid} parent={parent} />
+    })
   }
-
-  componentWillMount() {
-    AppStore.addChangeListener(this.onChange)
-  }
-
-  componentDidMount() {
-    AppActions.getParents()
-  }
-
-  componentWillUnmount() {
-    AppStore.removeChangeListener(this.onChange)
-  }
-
-  onChange = () => this.setState({ parents: AppStore.getParents() })
-
-  render() {
-    let parentListItems
-
-    if (this.state.parents) {
-      parentListItems = this.state.parents.map(parent => {
-        return <ParentListItem key={parent._id.$oid} parent={parent} />
-      })
-    }
-    return <div>{parentListItems}</div>
-  }
+  return <div>{parentListItems}</div>
 }
 
 export default Parents
