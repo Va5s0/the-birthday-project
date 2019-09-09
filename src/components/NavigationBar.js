15f3d48c202c2485a9a8726fc9f4d6b75ccc17d7
Va5s0 <vassog@gmail.com>
2019-09-09 18:19:43 +0300

    fix navigation bar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,47 +1,29 @@
 import React from "react"
-import { Navbar } from "react-bootstrap"
+import { css, cx } from "emotion"
 
-// import { injectGlobal } from "emotion"
-
-// injectGlobal`
-//   .navbar-inverse {
-//       background-color: transparent !important;
-//       .navbar-brand {
-//         color: #333 !important;
-//         font-size: 24px !important;
-//         margin-top: 40px;
-//       }
-//     },
-// `
+const classNames = {
+  navbarInverse: css`
+      background-color: transparent;
+      .navbar-brand {
+        color: #333;
+        font-size: 24px;
+        margin-top: 40px;
+      `,
+  containerFluid: css`
+    padding-left: 50px;
+  `,
+}
 
 const NavigationBar = () => (
-  <Navbar inverse collapseOnSelect>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <a href="#brand">The BirthDay Project</a>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    {/* <Navbar.Collapse>
-      <Nav>
-        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-          <MenuItem eventKey={3.1}>Action</MenuItem>
-          <MenuItem eventKey={3.2}>Another action</MenuItem>
-          <MenuItem eventKey={3.3}>Something else here</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey={3.3}>Separated link</MenuItem>
-        </NavDropdown>
-      </Nav>
-      <Nav pullRight>
-        <NavItem eventKey={1} href="#">
-          Link Right
-        </NavItem>
-        <NavItem eventKey={2} href="#">
-          Link Right
-        </NavItem>
-      </Nav>
-    </Navbar.Collapse> */}
-  </Navbar>
+  <nav className={cx([classNames.navbarInverse, "navbar"])}>
+    <div className={classNames.containerFluid}>
+      <div className="navbar-header">
+        <a className="navbar-brand" href="#brand">
+          The BirthDay Project
+        </a>
+      </div>
+    </div>
+  </nav>
 )
 
 export default NavigationBar
