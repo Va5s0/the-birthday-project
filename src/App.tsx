ecf70c774492a64cad1adaec791ddf25ca9def80
vasso <vasso@vassos-MacBook-Pro.local>
2022-04-07 00:49:09 +0300

    fix: routes conflict issue

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,12 @@ import { firebase } from "./firebase/fbConfig"
 import Base from "./routes/Base"
 import { history } from "./my-history"
 import { Theme } from "providers/Theme"
-import Auth from "routes/Auth"
 import { ProvideAuth } from "context/AuthContext"
+import { Login } from "routes/Auth/Login"
+import { actions } from "routes/Auth/utils"
+import { Landing } from "routes/Auth/Landing"
+import { ResetConfirmation } from "routes/Auth/ResetConfirmation"
+import { Forgot } from "routes/Auth/Forgot"
 
 export function App() {
   return (
@@ -14,7 +18,26 @@ export function App() {
         <ProvideAuth>
           <Router history={history}>
             <Switch>
-              <Route path="/login" component={Auth} />
+              <Route path="/reset">
+                <Landing>
+                  <ResetConfirmation />
+                </Landing>
+              </Route>
+              <Route path="/forgot">
+                <Landing>
+                  <Forgot />
+                </Landing>
+              </Route>
+              <Route path="/signup">
+                <Landing>
+                  <Login action={actions["signup"]} />
+                </Landing>
+              </Route>
+              <Route path="/login">
+                <Landing>
+                  <Login action={actions["login"]} />
+                </Landing>
+              </Route>
               <Route component={Base} />
             </Switch>
           </Router>
